981be14937c58941cbb91ca749ee5065cd4e734c
reznikkvova <reznikvova2222@gmail.com>
2022-11-11 13:12:27 +0200

    fx

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
+    $('.slider-body').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        autoplay: true,
+        autoplaySpeed: 2500,
+    });
     const burger = document.querySelector('.header-burger');
     burger.addEventListener('click', () => {
         burger.classList.toggle('active');
